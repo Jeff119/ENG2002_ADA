dc5e7cd6b180936964f13275c1e0d90b2d794756
DESKTOP-GB4B3LU\jefff <jefffung20021109@gmail.com>
2021-11-20 10:06:44 +0800

    adjustment

--- main/main/complexValue.h
+++ main/main/complexValue.h
@@ -1,20 +1,20 @@
 class complexNum
 {
 public:
-	complexNum(double r, double i);
-	~complexNum();
-	void SetReNum(double Re);
-	void SetImNum(double Im);
-	double GetReNum();
-	double GetImNum();
-	bool ReaderrNum();
-	bool checkVaild();
-	complexNum division(complexNum obj, complexNum obj1); 
-	complexNum squareroot(complexNum);
+	complexNum(double r, double i); //constructor 
+	~complexNum(); //destructor
+	void SetReNum(double r); //set the real part only
+	void SetImNum(double i); //set the imaginary part only
+	double GetReNum(); //show the real part
+	double GetImNum(); //show the imaginary part
+	bool ReaderrNum(); //read the value of errNum
+	bool checkVaild(); //check the operand
+	complexNum division(complexNum obj, complexNum obj1); //divsion of 2 complex number
+	complexNum squareroot(complexNum); //square root of a complex number
 
 private:
 	double x ; //real part
 	double y ; //imaginary part 
 	bool errNum; 
-	int sign; //store the value for the sign;
+
 };
